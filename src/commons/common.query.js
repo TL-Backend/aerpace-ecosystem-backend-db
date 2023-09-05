035d4fd06cc6457a0a354081f4062dac53584c10
Bharath Kandula <62982842+bharathkandula99@users.noreply.github.com>
2023-09-05 13:21:01 +0530

    Added pagination query to DB repo

---
+++ src/commons/common.query.js
@@ -0,0 +1,5 @@
+const {defaultValues} = require("./constant");
+
+exports.getPaginationQuery = (pageLimit, pageNumber) => {
+    return `OFFSET((${parseInt(pageNumber || defaultValues.DEFAULT_PAGE_NUMBER)}-1)*${parseInt(pageLimit || defaultValues.DEFAULT_PAGE_LIMIT,)}) ROWS FETCH NEXT ${parseInt(pageLimit || defaultValues.DEFAULT_PAGE_LIMIT)} ROWS ONLY`;
+}
--- src/commons/constants.js
+++
@@ -1,7 +0,0 @@
-const constants = {
-  device_types: ["CAR", "DRONE"],
-  user_types: ["STAFF", "MEMBER"],
-  device_status: ["ACTIVE", "INACTIVE", "DRAFT"],
-  personalities: ["OWNER", "GUEST", "DRIVER"],
-  device_levels: ["MODEL","VARIANTS","VERSIONS"]
-}
