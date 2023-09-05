771b2df1118125c8911ab40aa9525d2cf44dfa1f
Pradeep-p73 <pradeep@tilicho.in>
2023-09-05 14:53:04 +0530

    refactored query function

--- src/commons/common.query.js
+++ src/commons/common.query.js
@@ -1,5 +1,11 @@
-const {defaultValues} = require("./constant");
+const { defaultValues } = require("./constant");
 
-exports.getPaginationQuery = (pageLimit, pageNumber) => {
-    return `OFFSET((${parseInt(pageNumber || defaultValues.DEFAULT_PAGE_NUMBER)}-1)*${parseInt(pageLimit || defaultValues.DEFAULT_PAGE_LIMIT,)}) ROWS FETCH NEXT ${parseInt(pageLimit || defaultValues.DEFAULT_PAGE_LIMIT)} ROWS ONLY`;
-}
+exports.getPaginationQuery = ({ pageLimit, pageNumber }) => {
+  return `OFFSET((${parseInt(
+    pageNumber || defaultValues.DEFAULT_PAGE_NUMBER
+  )}-1)*${parseInt(
+    pageLimit || defaultValues.DEFAULT_PAGE_LIMIT
+  )}) ROWS FETCH NEXT ${parseInt(
+    pageLimit || defaultValues.DEFAULT_PAGE_LIMIT
+  )} ROWS ONLY`;
+};
